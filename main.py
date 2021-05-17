6c8133abf1732a42ec87d8f019f3dd67dc564e3d
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-17 13:57:52 +0100

    Added answers

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import numpy as np
 
 def sample_mean(n) :
   # Code for generating the sample mean goes here
+  mm = 0
+  for i in range(n) : mm = mm + np.random.uniform(0,1)
+  return mm / n
 
 
 def sample(m,n) :
   # Code for generating the sample goes here
+  sample = np.zeros(m)
+  for i in range(m) : sample[i] = sample_mean(n)
+  return sample
