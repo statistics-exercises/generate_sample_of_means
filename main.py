3756e55c1afa5d94deb6b099a411f6a2913fc90f
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-17 13:54:58 +0100

    Added test

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def sample_mean(n) :
+  # Code for generating the sample mean goes here
+
+
+def sample(m,n)
+  # Code for generating the sample goes here
---
+++ testing/test_main.py
@@ -0,0 +1,29 @@
+try:
+    from AutoFeedback.funcchecks import check_func 
+except:
+    import subprocess
+    import sys
+            
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
+    from AutoFeedback.funcchecks import check_func 
+           
+from AutoFeedback.randomclass import randomvar
+import unittest
+from main import *
+
+class UnitTests(unittest.TestCase) :
+   def test_mean(self) :
+       inputs, var = [], []
+       for i in range(2,10) : 
+           inputs.append((i,))
+           myvar1 = randomvar( 0.5, variance=1/12/i, vmin=0, vmax=1, isinteger=False )
+           var.append(myvar1)
+       assert( check_func("sample_mean", inputs, var) ) 
+
+   def test_sample(self) : 
+       inputs, var = [], [] 
+       for i in range(10,20) :
+           for j in range(2,10) :
+               inputs.append((i,j))
+               myvar1 = randomvar( 0.5, variance=1/12/j, vmin=0, vmax=1, isinteger=False )
+       assert( check_func("sample", inputs, var, calls=["sample_mean"]) ) 
